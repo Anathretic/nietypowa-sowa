ee27a4aa12b91b3b2f78bc7a5a97b7381f966db8
Konrad <konrad.wojtylo.9@gmail.com>
2023-10-30 10:07:53 +0100

    Added description to each image in photos section

--- src/sections/Photos.jsx
+++ src/sections/Photos.jsx
@@ -12,11 +12,29 @@ export const Photos = () => {
 					<div className='flex mf:flex-row flex-col items-center justify-center md:p-20 py-12 px-4 min-h-screen'>
 						<div className='wrapper'>
 							<h2 className='special-title text-5xl rsm:text-6xl mf:text-7xl text-white capitalize py-10'>{id}</h2>
-							{photos.map(data => (
-								<div key={data.imageId} className='py-12'>
-									<img src={data.image} alt={data.alt} className='rounded-2xl shadow-2xl shadow-neutral-800 w-full' />
-								</div>
-							))}
+							<div className='flex items-center justify-center flex-col w-full max-w-[700px] text-white'>
+								{photos.map(data => (
+									<div key={data.imageId} className='py-12'>
+										<img src={data.image} alt={data.alt} className='rounded-2xl shadow-2xl shadow-neutral-800 w-full' />
+										<div className='text-center'>
+											<h3 className='pt-12 pb-8 text-2xl lg:text-3xl font-bold'>
+												<span className='hidden sm:inline'>•</span> {data.title}{' '}
+												<span className='hidden sm:inline'>•</span>
+											</h3>
+											<p className='pb-6 lg:text-lg'>{data.description}</p>
+											<div className='flex items-center justify-center flex-col lg:text-lg'>
+												{data.size && (
+													<span className='pb-6'>
+														<b>Wymiary: </b>
+														{data.size}
+													</span>
+												)}
+												<span className='font-bold'>Stworzono w {data.year} roku.</span>
+											</div>
+										</div>
+									</div>
+								))}
+							</div>
 							<div className='flex flex-row justify-center items-center'>
 								<Link
 									to='/oferta'
